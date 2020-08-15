9f4a8edb38386efbc0fca294a8bcd2d96cb63c25
Decebel Geduriagao <iamdecebel@gmail.com>
2020-08-15 21:43:33 +0900

    Updates

---
+++ precache-manifest.19829f90f5ad06878cd6f4689346b708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d964f62fba9c9d129b8179bfb1a2a7",
+    "url": "/aesthete/index.html"
+  },
+  {
+    "revision": "46bf78894501f2f5d5f7",
+    "url": "/aesthete/static/css/main.67cc647d.chunk.css"
+  },
+  {
+    "revision": "9b3966935114c21ad8b7",
+    "url": "/aesthete/static/js/2.18eb4dd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aesthete/static/js/2.18eb4dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46bf78894501f2f5d5f7",
+    "url": "/aesthete/static/js/main.1b0500f9.chunk.js"
+  },
+  {
+    "revision": "56b01c7fd1935809a0e8",
+    "url": "/aesthete/static/js/runtime-main.8010aaaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aesthete/static/media/logo.5d5d9eef.svg"
+  }
+]);
