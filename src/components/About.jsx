42fb7481e3cefde0f37069a81a3b712d8c2659d1
Decebel Geduriagao <iamdecebel@gmail.com>
2020-08-15 21:13:01 +0900

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,175 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div className="about">
+            {/* Main wrapper */}
+            <div className="wrapper" id="wrapper">
+                {/* Start Bradcaump area */}
+                <div className="ht__bradcaump__area bg-image--3">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="bradcaump__inner text-center">
+                                    <h2 className="bradcaump-title">About us</h2>
+                                    <nav className="bradcaump-content">
+                                        <a className="breadcrumb_item" href="index.html">Home</a>
+                                        <span className="brd-separetor">/</span>
+                                        <span className="breadcrumb_item active">About us</span>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* End Bradcaump area */}
+                {/* Start About Area */}
+                <div className="page-about about_area bg--white section-padding--lg">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="section__title--3 text-center pb--30">
+                                    <h2>Our Process Skill Of High</h2>
+                                    <p>the right people for your project</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row align-items-center">
+                            <div className="col-lg-6 col-sm-12 col-12">
+                                <div className="content text-left text_style--2">
+                                    <h2>we have skills to show</h2>
+                                    <div className="skill-container">
+                                        {/* Start single skill */}
+                                        <div className="single-skill">
+                                            <p>Customer Favorites</p>
+                                            <div className="progress">
+                                                <div className="progress-bar wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay=".5s" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} style={{ width: '90%' }}><span className="pen-lable" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* End single skill */}
+                                        {/* Start single skill */}
+                                        <div className="single-skill">
+                                            <p>Popular Authors</p>
+                                            <div className="progress">
+                                                <div className="progress-bar wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay=".5s" role="progressbar" aria-valuenow={95} aria-valuemin={0} aria-valuemax={100} style={{ width: '95%' }}><span className="pen-lable" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* End single skill */}
+                                        {/* Start single skill */}
+                                        <div className="single-skill">
+                                            <p>Bestselling Series</p>
+                                            <div className="progress">
+                                                <div className="progress-bar wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay=".5s" role="progressbar" aria-valuenow={93} aria-valuemin={0} aria-valuemax={100} style={{ width: '93%' }}><span className="pen-lable" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* End single skill */}
+                                        {/* Start single skill */}
+                                        <div className="single-skill">
+                                            <p>Bargain Books</p>
+                                            <div className="progress">
+                                                <div className="progress-bar wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay=".5s" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} style={{ width: '90%' }}><span className="pen-lable" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* End single skill */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-sm-12 col-12">
+                                <div className="content">
+                                    <h3>Buy Book</h3>
+                                    <h2>Different Knowledge</h2>
+                                    <p className="mt--20 mb--20">Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+                                    <strong>London Address</strong>
+                                    <p>34 Parer Place via Musk Avenue Kelvin Grove, QLD, 4059</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* End About Area */}
+                {/* Start Team Area */}
+                <section className="wn__team__area pb--75 bg--white">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="section__title--3 text-center">
+                                    <h2>Meet our team of experts</h2>
+                                    <p>the right people for your project</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            {/* Start Single Team */}
+                            <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div className="wn__team">
+                                    <div className="thumb">
+                                        <img src="images/about/team/1.jpg" alt="Team images" />
+                                    </div>
+                                    <div className="content text-center">
+                                        <h4>JOHN SMITH</h4>
+                                        <p>Manager</p>
+                                        <ul className="team__social__net">
+                                            <li><a href="#"><i className="icon-social-twitter icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-tumblr icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-facebook icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-linkedin icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-pinterest icons" /></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* End Single Team */}
+                            {/* Start Single Team */}
+                            <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div className="wn__team">
+                                    <div className="thumb">
+                                        <img src="images/about/team/2.jpg" alt="Team images" />
+                                    </div>
+                                    <div className="content text-center">
+                                        <h4>ALICE KIM</h4>
+                                        <p>Co-Founder</p>
+                                        <ul className="team__social__net">
+                                            <li><a href="#"><i className="icon-social-twitter icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-tumblr icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-facebook icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-linkedin icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-pinterest icons" /></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* End Single Team */}
+                            {/* Start Single Team */}
+                            <div className="col-lg-4 col-md-4 col-sm-6 col-12">
+                                <div className="wn__team">
+                                    <div className="thumb">
+                                        <img src="images/about/team/3.jpg" alt="Team images" />
+                                    </div>
+                                    <div className="content text-center">
+                                        <h4>VICTORIA DOE</h4>
+                                        <p>Marketer</p>
+                                        <ul className="team__social__net">
+                                            <li><a href="#"><i className="icon-social-twitter icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-tumblr icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-facebook icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-linkedin icons" /></a></li>
+                                            <li><a href="#"><i className="icon-social-pinterest icons" /></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* End Single Team */}
+                        </div>
+                    </div>
+                </section>
+                {/* End Team Area */}
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+function Footer() {
+    return (
+        <div className="footer">
+            {/* Main Wrapper */}
+            <div class="wrapper" id="wrapper">
+                {/* Footer Area */}
+                <footer id="wn__footer" className="footer__area bg__cat--8 brown--color">
+                    <div className="footer-static-top">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="footer__widget footer__menu">
+                                        <div className="ft__logo">
+                                            <a href="index.html">
+                                                <img src="/images/logo/3.png" alt="logo" />
+                                            </a>
+                                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered duskam alteration variations of passages</p>
+                                        </div>
+                                        <div className="footer__content">
+                                            <ul className="social__net social__net--2 d-flex justify-content-center">
+                                                <li><a href="#"><i className="bi bi-facebook" /></a></li>
+                                                <li><a href="#"><i className="bi bi-google" /></a></li>
+                                                <li><a href="#"><i className="bi bi-twitter" /></a></li>
+                                                <li><a href="#"><i className="bi bi-linkedin" /></a></li>
+                                                <li><a href="#"><i className="bi bi-youtube" /></a></li>
+                                            </ul>
+                                            <ul className="mainmenu d-flex justify-content-center">
+                                                <li><a href="index.html">Trending</a></li>
+                                                <li><a href="index.html">Best Seller</a></li>
+                                                <li><a href="index.html">All Product</a></li>
+                                                <li><a href="index.html">Wishlist</a></li>
+                                                <li><a href="index.html">Blog</a></li>
+                                                <li><a href="index.html">Contact</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="copyright__wrapper">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="copyright">
+                                        <div className="copy__right__inner text-left">
+                                            <p>Copyright <i className="fa fa-copyright" /> <a href="https://freethemescloud.com/">Free themes Cloud.</a> All Rights Reserved</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-sm-12">
+                                    <div className="payment text-right">
+                                        <img src="/images/icons/payment.png" alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                {/* //Footer Area */}
+                {/* QUICKVIEW PRODUCT */}
+                <div id="quickview-wrapper">
+                    {/* Modal */}
+                    <div className="modal fade" id="productmodal" tabIndex={-1} role="dialog">
+                        <div className="modal-dialog modal__container" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header modal__header">
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="modal-product">
+                                        {/* Start product images */}
+                                        <div className="product-images">
+                                            <div className="main-image images">
+                                                <img alt="big images" src="/images/product/big-img/1.jpg" />
+                                            </div>
+                                        </div>
+                                        {/* end product images */}
+                                        <div className="product-info">
+                                            <h1>Simple Fabric Bags</h1>
+                                            <div className="rating__and__review">
+                                                <ul className="rating">
+                                                    <li><span className="ti-star" /></li>
+                                                    <li><span className="ti-star" /></li>
+                                                    <li><span className="ti-star" /></li>
+                                                    <li><span className="ti-star" /></li>
+                                                    <li><span className="ti-star" /></li>
+                                                </ul>
+                                                <div className="review">
+                                                    <a href="#">4 customer reviews</a>
+                                                </div>
+                                            </div>
+                                            <div className="price-box-3">
+                                                <div className="s-price-box">
+                                                    <span className="new-price">$17.20</span>
+                                                    <span className="old-price">$45.00</span>
+                                                </div>
+                                            </div>
+                                            <div className="quick-desc">
+                                                Designed for simplicity and made from high quality materials. Its sleek geometry and material combinations creates a modern look.
+                                            </div>
+                                            <div className="select__color">
+                                                <h2>Select color</h2>
+                                                <ul className="color__list">
+                                                    <li className="red"><a title="Red" href="#">Red</a></li>
+                                                    <li className="gold"><a title="Gold" href="#">Gold</a></li>
+                                                    <li className="orange"><a title="Orange" href="#">Orange</a></li>
+                                                    <li className="orange"><a title="Orange" href="#">Orange</a></li>
+                                                </ul>
+                                            </div>
+                                            <div className="select__size">
+                                                <h2>Select size</h2>
+                                                <ul className="color__list">
+                                                    <li className="l__size"><a title="L" href="#">L</a></li>
+                                                    <li className="m__size"><a title="M" href="#">M</a></li>
+                                                    <li className="s__size"><a title="S" href="#">S</a></li>
+                                                    <li className="xl__size"><a title="XL" href="#">XL</a></li>
+                                                    <li className="xxl__size"><a title="XXL" href="#">XXL</a></li>
+                                                </ul>
+                                            </div>
+                                            <div className="social-sharing">
+                                                <div className="widget widget_socialsharing_widget">
+                                                    <h3 className="widget-title-modal">Share this product</h3>
+                                                    <ul className="social__net social__net--2 d-flex justify-content-start">
+                                                        <li className="facebook"><a href="#" className="rss social-icon"><i className="zmdi zmdi-rss" /></a></li>
+                                                        <li className="linkedin"><a href="#" className="linkedin social-icon"><i className="zmdi zmdi-linkedin" /></a></li>
+                                                        <li className="pinterest"><a href="#" className="pinterest social-icon"><i className="zmdi zmdi-pinterest" /></a></li>
+                                                        <li className="tumblr"><a href="#" className="tumblr social-icon"><i className="zmdi zmdi-tumblr" /></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div className="addtocart-btn">
+                                                <a href="#">Add to cart</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* END QUICKVIEW PRODUCT */}
+            </div>
+            <Helmet>
+                {/* JS Files */}
+                <script src="js/active.js"></script>
+            </Helmet>
+        </div>
+    )
+}
+
+export default Footer;
