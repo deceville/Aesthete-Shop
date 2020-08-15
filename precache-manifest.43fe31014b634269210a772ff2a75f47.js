a51ca44043b01eae3d1fbe465051847b6a0e6e23
Decebel Geduriagao <iamdecebel@gmail.com>
2020-08-15 22:17:22 +0900

    Updates

---
+++ precache-manifest.43fe31014b634269210a772ff2a75f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db40886ee690fcd33f52a5771876b5ef",
+    "url": "/aesthete-shop/index.html"
+  },
+  {
+    "revision": "5004d7a904fb34b55664",
+    "url": "/aesthete-shop/static/css/main.67cc647d.chunk.css"
+  },
+  {
+    "revision": "caae073749032ae21a82",
+    "url": "/aesthete-shop/static/js/2.b29ec982.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aesthete-shop/static/js/2.b29ec982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5004d7a904fb34b55664",
+    "url": "/aesthete-shop/static/js/main.95dfbdec.chunk.js"
+  },
+  {
+    "revision": "b7d9b33ce3fc86c1e22d",
+    "url": "/aesthete-shop/static/js/runtime-main.137656d8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aesthete-shop/precache-manifest.3324b97fb98e302b1f9a871478744243.js"
+  "/aesthete-shop/precache-manifest.43fe31014b634269210a772ff2a75f47.js"
 );
 
 self.addEventListener('message', (event) => {
